6ed3e578606865cc57f764f72c0a3c24a153f288
Anand kumar <anandkumar.triden@gmail.com>
2023-08-12 16:55:42 +0530

    crousel update for size and possition by anand on 2023-08-12

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { clients,support } from "../constants";
+import { clients, support } from "../constants";
 import styles from "../style";
 
 const Carousel = ({ items }) => {
@@ -26,14 +26,16 @@ const Carousel = ({ items }) => {
 
   return (
     <div className="relative w-full overflow-hidden">
-      <div className="carousel flex transition-transform duration-500 ease-in-out" style={{ transform: `translateX(-${activeIndex * 100}%)` }}>
+      <div
+        className="carousel flex transition-transform duration-500 ease-in-out"
+        style={{ transform: `translateX(-${activeIndex * 100}%)` }}
+      >
         {items.map((item, index) => (
           <div key={index} className="w-full flex-shrink-0">
             <img src={item.logo} alt={item.id} className="w-70 h-auto" />
           </div>
         ))}
       </div>
-      
     </div>
   );
 };
@@ -42,32 +44,47 @@ const Clients = () => {
   // Define your clients' data. For example:
 
   return (
-    <section className={` ${styles.flexCenter}my-4 `}>
-      <div className="crousel1"
-      style={{marginLeft:"100px"}}
-      >
-      <h3 className="font-poppins font-bold text-[10px] leading-[17px] text-white">Recognised by</h3>
+    <section className={`${styles.flexCenter} my-4`}>
+      <div className="crousel1" style={{ marginLeft: "50px", marginRight: "6px" }}>
+        <h3 className="font-poppins font-semibold text-[15px] md:text-[10px] leading-[17px] text-white">Recognise by</h3>
+        <div
+          className={`${styles.flexCenter} w-full relative`}
+          style={{
+            boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+            backgroundColor: "ActiveCaption",
+            borderRadius: "20px",
+            border: "1px solid #33DFF0",
+            padding: "10px", // Add padding to give space around the carousel content
+          }}
+        >
+          {/* Use the Carousel component here */}
+          <Carousel items={clients} />
+        </div>
+      </div>
       <div
-        className={`${styles.flexCenter} w-full relative`}
-        style={{ width:"95%",marginRight: "10px", boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)", backgroundColor:"ActiveCaption", borderRadius: "20px",border: "1px solid #33DFF0",}}
+        className="crousel2"
+        style={{
+          marginLeft: "auto", // Center the second carousel horizontally
+          marginRight: "auto",
+          marginTop: "auto",        }}
       >
-        
-        {/* Use the Carousel component here */}
-        <Carousel items={clients} />
-      </div>
-      </div>
-      <div className="crousel2"
-      style={{marginLeft:"5px"}}
-      >
-      <h3 className="font-poppins font-bold text-[10px] leading-[17px] text-white">Supported by</h3>
-      <div
-        className={`${styles.flexCenter} w-full relative`}
-        style={{ width: "33%", boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",backgroundColor:"ActiveCaption", borderRadius: "20px",border: "1px solid #33DFF0",}}
-      >
-
-        {/* Use the Carousel component here */}
-        <Carousel items={support} />
-      </div>
+        <h3 className="font-poppins font-semibold text-[15px] md:text-[10px] leading-[17px] text-white" style={{marginLeft:"30%"}}>Supported by</h3>
+        <div
+          className={`${styles.flexCenter} w-full relative`}
+          style={{
+            maxWidth: "40%", // Limit the maximum width for better responsiveness
+            width: "37%",
+            boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+            backgroundColor: "ActiveCaption",
+            borderRadius: "20px",
+            border: "1px solid #33DFF0",
+            padding: "10px", // Add padding to give space around the carousel content
+            margin: "0 auto", // Center the carousel horizontally
+          }}
+        >
+          {/* Use the Carousel component here */}
+          <Carousel items={support} />
+        </div>
       </div>
     </section>
   );
